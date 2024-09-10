f5eeb8c22c1c381096a85fb97e5a8302ff4a8e7f
Ekuesky <ayiekue9127@gmail.com>
2024-09-10 08:17:56 +0000

    add logging to config

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -183,3 +183,21 @@ CELERY_BEAT_SCHEDULE = {
         "task": "update_all_reputations",
     }
 }
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)s %(name)-12s %(asctime)s %(module)s %(process)d %(thread)d %(message)s"
+        }
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        }
+    },
+    "root": {"level": "INFO", "handlers": ["console"]},
+}
