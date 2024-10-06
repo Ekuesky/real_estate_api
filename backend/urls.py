bec1b433790d21246bfb67f956b7080c51799b56
Ekuesky <ayiekue9127@gmail.com>
2024-10-06 21:52:48 +0000

    feat:profile views and urls

--- backend/urls.py
+++ backend/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
     path("api/v1/auth/", include("djoser.urls")),
     path("api/v1/auth/", include("core_apps.users.urls")),
-    # path("api/v1/profiles/", include("core_apps.profiles.urls")),
+    path("api/v1/profiles/", include("core_apps.profiles.urls")),
     # path("api/v1/apartments/", include("core_apps.apartments.urls")),
     # path("api/v1/issues/", include("core_apps.issues.urls")),
     # path("api/v1/reports/", include("core_apps.reports.urls")),
