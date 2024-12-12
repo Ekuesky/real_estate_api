a3cbbc8f5711be5e4f3501431b0c5b7d90d03b9f
Ekuesky <ayiekue9127@gmail.com>
2024-12-12 09:18:59 +0000

    refactor in Issues module

--- core_apps/issues/admin.py
+++ core_apps/issues/admin.py
@@ -5,11 +5,12 @@ from core_apps.common.models import ContentView
 from .models import Issue
 from core_apps.users.models import User
 from django.db.models import Q
-from django.forms import ModelForm  # Import ModelForm
-from .emails import send_resolution_email
+from django.forms import ModelForm
+from .emails import send_resolution_email, send_issue_confirmation_email
 from django.utils import timezone
 
-class IssueForm(ModelForm):  # Create a custom form class
+
+class IssueForm(ModelForm):
     class Meta:
         model = Issue
         fields = "__all__"
@@ -17,14 +18,18 @@ class IssueForm(ModelForm):  # Create a custom form class
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Customize 'assigned_to' queryset (Filter in the Form)
-        self.fields["assigned_to"].queryset = User.objects.exclude(
-            Q(is_superuser=True) | Q(profile__occupation="tenant") | Q(is_active=False)
-        )
-        # Customize 'reported_by' queryset (Filter in the Form)
-        self.fields["reported_by"].queryset = User.objects.filter(
-            Q(is_active=True) & (Q(is_superuser=True) | Q(is_staff=True) | Q(profile__occupation__iexact="tenant"))
-        )
+
+        # Check if the fields exist before filtering
+        if "assigned_to" in self.fields:
+            self.fields["assigned_to"].queryset = User.objects.exclude(
+                Q(is_superuser=True) | Q(profile__occupation="tenant") | Q(is_active=False)
+            )
+
+        if "reported_by" in self.fields:
+            self.fields["reported_by"].queryset = User.objects.filter(
+                Q(is_active=True) & (Q(is_superuser=True) | Q(is_staff=True) | Q(profile__occupation__iexact="tenant"))
+            )
+
 
 @admin.register(Issue)
 class IssueAdmin(admin.ModelAdmin):
@@ -39,12 +44,19 @@ class IssueAdmin(admin.ModelAdmin):
     ]
     list_display_links = ["id", "apartment"]
     list_filter = ["status", "priority"]
-    search_fields = ["apartment__unit_number", "reported_by__first_name"]
+    search_fields = ["apartment__unit_number", "reported_by__first_name", "reported_by__last_name"]
     ordering = ["-created_at"]
     autocomplete_fields = ["apartment"]
     inlines = [ContentViewInline]
-    form = IssueForm  # use the custom form
-    readonly_fields = []
+    readonly_fields = ["resolved_on"]
+    form = IssueForm
+
+    def has_change_permission(self, request, obj=None):
+        # Interdire la modification si l'objet existe et son statut est RESOLVED
+        if obj and obj.status == Issue.IssueStatus.RESOLVED:
+            return False
+        return super().has_change_permission(request, obj)
+
     def get_total_views(self, obj):
         content_type = ContentType.objects.get_for_model(obj)
         views = ContentView.objects.filter(
@@ -53,7 +65,11 @@ class IssueAdmin(admin.ModelAdmin):
         return views
 
     def save_model(self, request, obj, form, change):
-        if 'status' in form.changed_data:
+        is_new = not change
+        if is_new:
+            obj.status = Issue.IssueStatus.REPORTED  # Set the status to REPORTED for a new issue
+            send_issue_confirmation_email(obj)
+        elif 'status' in form.changed_data:
             if obj.status == Issue.IssueStatus.RESOLVED and form.initial['status'] != Issue.IssueStatus.RESOLVED:
                 obj.resolved_on = timezone.now().date()
                 obj.resolved_by = request.user
